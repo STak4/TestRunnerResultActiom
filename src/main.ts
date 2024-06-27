b68930902a48795a8cd7a984ba5a8b896ca37e41
STak4 <ARTAhsv010Lain>
2024-06-28 04:23:48 +0900

    update: Scripts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import * as core from '@actions/core'
-import { wait } from './wait'
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import { wait } from "./wait";
 
 /**
  * The main function for the action.
@@ -7,20 +8,29 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    const myInput = core.getInput("myInput");
+    core.debug(`Hello ${myInput}!`);
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    core.debug(`The event payload: ${payload}`);
   } catch (error) {
     // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) core.setFailed(error.message);
   }
+  // try {
+  //   const ms: string = core.getInput('milliseconds')
+
+  //   // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
+  //   core.debug(`Waiting ${ms} milliseconds ...`)
+
+  //   // Log the current timestamp, wait, then log the new timestamp
+  //   core.debug(new Date().toTimeString())
+  //   await wait(parseInt(ms, 10))
+  //   core.debug(new Date().toTimeString())
+
+  //   // Set outputs for other workflow steps to use
+  //   core.setOutput('time', new Date().toTimeString())
+  // } catch (error) {
+  //   // Fail the workflow run if an error occurs
+  //   if (error instanceof Error) core.setFailed(error.message)
+  // }
 }
